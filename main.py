9a9b321137935824f2e558995d606b2e583aade2
Vlakin69 <vlados19.2002@gmail.com>
2022-04-18 15:13:26 +0300

    scnd commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import asyncio
 from datetime import datetime
 from parser_script import schedule_parser
 from rich import print
-#log level
+#log level6
 logging.basicConfig(level=logging.INFO)
 
 #bot init
